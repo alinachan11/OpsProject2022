1d6ffb3a05e4b2493e6b77098b5f1b79f4f23b3b
alinachan11 <alinachan460@gmail.com>
2022-01-02 02:23:46 +0200

    trying eks

--- output.tf
+++ output.tf
@@ -10,5 +10,28 @@ output "consul_lb_address" {
     value = module.sd_module.consul_lb_dns
 }
 
+################# EKS Output ###########################
+/*
+output "cluster_name" {
+    value = module.EKS_Module.cluster_name
+}
+output "config_map_aws_auth" {
+    value = module.EKS_Module.config_map_aws_auth
+}
+output "oidc_provider_arn" {
+    value = module.EKS_Module.oidc_provider_arn
+}
+output "cluster_security_group_id" {
+    value = module.EKS_Module.cluster_security_group_id
+}
+output "cluster_endpoint" {
+    value = module.EKS_Module.cluster_endpoint
+}
+output "cluster_id" {
+    value = module.EKS_Module.cluster_id
+}
+output "cluster_region" {
+    value = module.EKS_Module.region
+}
 
-
+*/
