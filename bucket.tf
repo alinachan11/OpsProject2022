2fe14d029bcb8c0507a0743f44addc498299efc3
alinachan11 <alinachan460@gmail.com>
2022-02-20 01:10:21 +0200

    added s3 bucket acl resource

--- bucket.tf
+++ bucket.tf
@@ -1,8 +1,11 @@
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "alina-bucket-for-opsproject"
-  acl    = "private"
-
   tags = {
     Name = "alina-bucket"
   }
+}
+
+resource "aws_s3_bucket_acl" "my_bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.id
+  acl    = "private"
 }
