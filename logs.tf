34740797f95b21b0d5564f8229c6c637d9eb6354
alinachan11 <alinachan460@gmail.com>
2022-03-17 03:21:24 +0200

    made elk a lb

--- logs.tf
+++ logs.tf
@@ -10,14 +10,13 @@ module "ec2-instance" {
   instance_type               = "t3.medium"
   ami                         = data.aws_ami.ami.id
   key_name                    = var.ssh_key_name
-  subnet_id                   = tolist(module.vpc_module.public_subnets_id)[1]
+  subnet_id                   = tolist(module.vpc_module.private_subnets_id)[1]
   vpc_security_group_ids      = [aws_security_group.elk-sg.id,aws_security_group.ssh-sg.id,aws_security_group.default.id,aws_security_group.consul-sg.id]
   associate_public_ip_address = true
   user_data = file("./loguserdata.sh")
 
   tags = {
     Terraform   = "true"
-    Environment = "dev"
   }
 }
 
@@ -65,12 +64,12 @@ module "security-group" {
 
 ##################################################################
 
-output "public_ip" {
-  value = module.ec2-instance.public_ip[0]
-}
-output "kibana_url" {
-  value = "http://${module.ec2-instance.public_ip[0]}:5601"
-}
+#output "public_ip" {
+#  value = module.ec2-instance.public_ip[0]
+#}
+#output "kibana_url" {
+ # value = "http://${module.ec2-instance.public_ip[0]}:5601"
+#}
 
 ######################################################################
 
@@ -85,4 +84,53 @@ variable "prefix_name" {
 variable "instance_count" {
   type    = number
   default = 1
+}
+
+
+###########################################################################
+
+resource "aws_lb" "elk_alb" {
+  name               = "elk-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            =  module.vpc_module.public_subnets_id
+  security_groups    = [aws_security_group.elk-sg.id,aws_security_group.ssh-sg.id,aws_security_group.default.id,aws_security_group.consul-sg.id]
+
+  tags = {
+    "Name" = "elk-alb-${module.vpc_module.vpc_id}"
+  }
+}
+
+
+resource "aws_lb_listener" "elk" {
+  load_balancer_arn = aws_lb.elk_alb.arn
+  port              = 5601
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.elk.arn
+  }
+}
+
+resource "aws_lb_target_group" "elk" {
+  name     = "elk-target-group"
+  port     = 5601
+  protocol = "HTTP"
+  vpc_id   = module.vpc_module.vpc_id
+
+  health_check {
+    enabled = true
+    path    = "/"
+  }
+
+  tags = {
+    "Name" = "elk_target_group_${module.vpc_module.vpc_id}"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "jenkins_master" {
+  target_group_arn = aws_lb_target_group.jenkins_master.id
+  target_id        = module.ec2-instance.id
+  port             = 8080
 }
