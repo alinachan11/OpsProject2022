f5ef0eed69c87a00bb426db802b619f0882e23f3
alinachan11 <alinachan460@gmail.com>
2022-03-23 00:58:22 +0200

    fixed a problem with IAM

--- IAM.tf
+++ IAM.tf
@@ -104,9 +104,9 @@ resource "aws_iam_role" "kandula_role" {
   assume_role_policy  = "${file("policies/assumerolepolicy.json")}"
 }
 
-resource "aws_iam_instance_profile" "assume_role_profile" {                             
+resource "aws_iam_instance_profile" "assume_role_kandula" {                             
   name  = "assume_role_profile"                         
-  role = "${aws_iam_role.ansible_role.name}"
+  role = "${aws_iam_role.kandula_role.name}"
 }
 
 resource "aws_iam_policy" "kandula_role_access_policy" {
