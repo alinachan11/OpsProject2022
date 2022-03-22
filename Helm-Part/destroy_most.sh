fbad0c4a0c0e9ab5bb18b2a08c921f4f4d2fd7f7
alinachan11 <alinachan460@gmail.com>
2022-03-23 00:47:14 +0200

    adding basic hpa + k8s secrets + metrics server

--- Helm-Part/destroy_most.sh
+++ Helm-Part/destroy_most.sh
@@ -4,9 +4,6 @@ set -e
 echo "starting destruction of deployments...."
 aws eks --region=us-east-1 update-kubeconfig --name opsschool-eks-alina--final-project
 
-echo "starting destruction of filebeat...."
-kubectl delete -f /home/ubuntu/Ansible-Part/deployments/filebeat_deployment.yml
-
 echo "starting destruction of grafana_datasources...."
 kubectl delete -f /home/ubuntu/Ansible-Part/deployments/grafana_datasources.yml
 
@@ -24,5 +21,14 @@ helm delete myprom -n monitoring
 
 echo "starting destruction of grafana...."
 helm delete mygrafana -n monitoring
+
+echo "starting destruction of filebeat...."
+kubectl delete -f /home/ubuntu/Ansible-Part/deployments/filebeat_deployment.yml
+
+echo "starting destruction of fmetrics-server...."
+kubectl delete -f /home/ubuntu/Ansible-Part/deployments/metrics-server.yaml
+
+echo "starting destruction of hpa kandula...."
+kubectl delete -f /home/ubuntu/Ansible-Part/deployments/hpa_kandula.yml
 
 echo "end of destruction of deployments...."
---
+++ for_k8s.tf
@@ -0,0 +1,23 @@
+###############################################################@
+
+
+
+
+
+
+
+
+################################################################
+
+resource "kubernetes_secret_v1" "kandula_secrets" {
+  metadata {
+    name = "kandula-secrets"
+    namespace = "default"
+  }
+
+  data = {
+    aws_username = aws_iam_role.kandula_role.id
+    aws_password = aws_iam_role.kandula_role.id
+  }
+
+}
