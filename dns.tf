b7d5d0d30236ceaaa3213ecfa2d6bc9b213afa90
alinachan11 <alinachan460@gmail.com>
2022-03-25 22:19:01 +0300

    added dns changes

--- dns.tf
+++ dns.tf
@@ -27,8 +27,8 @@ resource "aws_route53_record" "myJenkinsRecord" {
   type    = "A" 
   ttl = "300"
   alias {
-      name                   = module.jenkins_module.consul_lb_dns
-      zone_id                = module.jenkins_module.consul_lb_zone_id
+      name                   = module.jenkins_module.jenkins_lb_dns
+      zone_id                = module.jenkins_module.jenkins_lb_zone_id
       evaluate_target_health = true
   }
 }
@@ -43,4 +43,61 @@ resource "aws_route53_record" "myELKRecord" {
       zone_id                = aws_lb.elk_alb.zone_id
       evaluate_target_health = true
   }
+}
+
+
+#############################################################################
+
+provider "acme" {
+  server_url = var.acme_server_url
+}
+
+resource "tls_private_key" "registration" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "acme_registration" "registration" {
+  account_key_pem = tls_private_key.registration.private_key_pem
+  email_address   = var.email_address
+
+  dynamic "external_account_binding" {
+    for_each = var.external_account_binding != null ? [null] : []
+    content {
+      key_id      = var.external_account_binding.key_id
+      hmac_base64 = var.external_account_binding.hmac_base64
+    }
+  }
+}
+
+resource "acme_certificate" "certificates" {
+  for_each = { for certificate in var.certificates : index(var.certificates, certificate) => certificate }
+
+  common_name               = each.value.common_name
+  subject_alternative_names = each.value.subject_alternative_names
+  key_type                  = each.value.key_type
+  must_staple               = each.value.must_staple
+  min_days_remaining        = each.value.min_days_remaining
+  certificate_p12_password  = each.value.certificate_p12_password
+  account_key_pem              = acme_registration.registration.account_key_pem
+  recursive_nameservers        = var.recursive_nameservers
+  disable_complete_propagation = var.disable_complete_propagation
+  pre_check_delay              = var.pre_check_delay
+
+  dns_challenge {
+      provider = var.dns_challenge_provider
+      config   = {
+        AWS_ACCESS_KEY_ID     = AWS_ACCESS_KEY_ID
+        AWS_SECRET_ACCESS_KEY = AWS_SECRET_ACCESS_KEY
+        AWS_DEFAULT_REGION    = var.aws_region
+        AWS_HOSTED_ZONE_ID    = aws_route53_zone.private.id
+      }
+    }
+}
+
+resource "aws_acm_certificate" "cert" {
+  private_key        =  acme_certificate.certificates[0].private_key_pem   
+  certificate_body   =  acme_certificate.certificates[0].certificate_pem 
+  certificate_chain  =  acme_certificate.certificates[0].issuer_pem
+  depends_on         =  [acme_certificate.certificates,tls_private_key.registration,acme_registration.registration]
 }
